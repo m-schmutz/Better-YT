9724be07e158ec2ca9f2c4abb7f1be684ac949b9
m-schmutz <marschm131@gmail.com>
2026-03-18 09:17:33 -0700

    pc stuff

--- test.py
+++ test.py
@@ -1,22 +1,21 @@
-#!./env/bin/python3
+#!/usr/bin/python3
 
-from lib.ytdlp import get_video_metadata, download_video
-
-
-
-resonanceUrl = 'https://www.youtube.com/watch?v=8GW6sLrK40k'
-
-url = 'https://www.youtube.com/watch?v=T-VZ1kL8ZgE&t'
-# url = 'https://www.youtube.com/watch?v=T-VZ1kLasdE&t'
+from lib.ytdlp import update_dlp_environment, get_video_metadata, download_video, download_audio
 
 
 
-meta = get_video_metadata(url)
-
-
-print(meta)
-
-download_video(resonanceUrl, 'resonance.mp4')
+resonanceUrl = 'https://www.youtube.com/watch?v=LZ4ug8Nc0Q8'
 
 
 
+update_dlp_environment()
+
+
+# metadata = get_video_metadata(resonanceUrl)
+
+
+# print(metadata)
+
+# download_video(resonanceUrl, 'resonance.mp4')
+
+download_audio(resonanceUrl, 'resonance.mp3')
